13f4caaef93b645f9354b1aac906bfdaa27b0f63
Robert Andrzejczuk <r.andrzejczuk2@gmail.com>
2021-11-13 14:31:45 +0100

    Repo cleanup

--- Checkers/bin/Release/netcoreapp3.1/publish/wwwroot/js/signalrReqHandlerRoom.js
+++
@@ -1,9 +0,0 @@
-﻿var connection = new signalR.HubConnectionBuilder()
-    .withUrl('/RoomHub')
-    .build();
-
-connection.on('realizeMovement', realizeMovement);
-connection.on('refresh', refresh);
-connection.on('updateTime', updateTime);
-connection.on('gameOver', gameOver);
-
